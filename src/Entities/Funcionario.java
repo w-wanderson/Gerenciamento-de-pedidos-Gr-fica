6fad9ba17c4fb14269d4e5f271b932ec12eb59c6
w-wanderson <wandersondasilvasantos18@hotmail.com>
2022-06-19 21:29:34 -0300

    Pedido

--- src/Entities/Funcionario.java
+++ src/Entities/Funcionario.java
@@ -1,5 +1,45 @@
 package Entities;
 
+import Enum.Cargo;
+
 public class Funcionario {
+	
+	private String nome;
+	private String cpf;
+	private Cargo cargo;
+	private double salario;
+	
+
+	
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Cargo getCargo() {
+		return cargo;
+	}
+	public void setCargo(Cargo cargo) {
+		this.cargo = cargo;
+	}
+	public double getSalario() {
+		return salario;
+	}
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+	
+	
+	
 
 }
